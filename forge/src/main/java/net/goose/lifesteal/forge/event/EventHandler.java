8ebb01c1570bb2ed73d7e3dccfb5e167897c586d
XariZaru <jonsnguyening@gmail.com>
2023-01-23 20:42:37 -0800

    Cleaned

--- forge/src/main/java/net/goose/lifesteal/forge/event/EventHandler.java
+++ forge/src/main/java/net/goose/lifesteal/forge/event/EventHandler.java
@@ -1,17 +1,11 @@
 package net.goose.lifesteal.forge.event;
 
-import net.goose.lifesteal.LifeSteal;
-import net.goose.lifesteal.client.render.ReviveHeadBER;
 import net.goose.lifesteal.command.ModCommands;
 import net.goose.lifesteal.data.HealthData;
-import net.minecraft.client.renderer.blockentity.SkullBlockRenderer;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.level.block.entity.BlockEntityType;
-import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
 
 @SuppressWarnings("unused")
 public class EventHandler {
@@ -19,6 +13,7 @@ public class EventHandler {
     public static void OnCommandsRegister(RegisterCommandsEvent event) {
         ModCommands.registerCommands(command -> command.accept(event.getDispatcher()));
     }
+
     @SubscribeEvent
     public static void playerCloneEvent(final PlayerEvent.Clone event) {
 
