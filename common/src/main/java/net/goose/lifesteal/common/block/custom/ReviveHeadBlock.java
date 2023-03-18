0c239aa2ef664be10c493713764b6ae680e3aa48
LoxiGoose <jonsnguyening@gmail.com>
2023-03-18 14:15:20 -0700

    test

--- common/src/main/java/net/goose/lifesteal/common/block/custom/ReviveHeadBlock.java
+++ common/src/main/java/net/goose/lifesteal/common/block/custom/ReviveHeadBlock.java
@@ -40,7 +40,7 @@ public class ReviveHeadBlock extends SkullBlock implements EntityBlock {
                 }
             }
 
-            skullBlockEntity.setOwner(gameProfile);
+            skullBlockEntity.setOwner(gameProfile);d
         }
     }
 }
