2a811c8900bc7acb9a17da59f48f56e643103024
LoxiGoose <docgoosenn@gmail.com>
2024-06-30 19:57:20 -0700

    Fixed liquids from breaking the revive head and duping it if on indestructible

--- common/src/main/java/net/goose/lifesteal/common/block/custom/ReviveHeadBlock.java
+++ common/src/main/java/net/goose/lifesteal/common/block/custom/ReviveHeadBlock.java
@@ -4,17 +4,37 @@ import net.goose.lifesteal.common.blockentity.custom.ReviveSkullBlockEntity;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.EntityBlock;
-import net.minecraft.world.level.block.SkullBlock;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.block.state.properties.EnumProperty;
+import net.minecraft.world.level.block.state.properties.Property;
+import net.minecraft.world.level.material.Fluid;
+import net.minecraft.world.level.material.FluidState;
 import org.jetbrains.annotations.Nullable;
 
-public class ReviveHeadBlock extends SkullBlock implements EntityBlock {
+public class ReviveHeadBlock extends SkullBlock implements EntityBlock, LiquidBlockContainer {
+
     public ReviveHeadBlock(Properties properties) {
         super(Types.PLAYER, properties);
+    }
+
+    @Override
+    public boolean canPlaceLiquid(@Nullable Player player, BlockGetter level, BlockPos pos, BlockState state, Fluid fluid) {
+        return false;
+    }
+
+    @Override
+    public boolean placeLiquid(LevelAccessor level, BlockPos pos, BlockState state, FluidState fluidState) {
+        return false;
     }
 
     @Override
