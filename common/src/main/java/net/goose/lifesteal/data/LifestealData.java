4e08830c9a573cffe3453bd01f21a2adcd2a69ed
LoxiGoose <docgoosenn@gmail.com>
2024-07-08 22:48:20 -0700

    datamap fix

--- common/src/main/java/net/goose/lifesteal/data/LifestealData.java
+++ common/src/main/java/net/goose/lifesteal/data/LifestealData.java
@@ -43,9 +43,9 @@ public class LifestealData implements ILifestealData {
     private final HashMap<ResourceLocation, Object> dataMap = new HashMap<>();
     private final LivingEntity livingEntity;
     public LifestealData(final LivingEntity entity) {
+        this.livingEntity = entity;
         this.dataMap.putIfAbsent(ModResources.HEALTH_DIFFERENCE, LifeSteal.config.startingHealthDifference.get());
         this.dataMap.putIfAbsent(ModResources.TIME_KILLED, 0L);
-        this.livingEntity = entity;
     }
 
     @ExpectPlatform
