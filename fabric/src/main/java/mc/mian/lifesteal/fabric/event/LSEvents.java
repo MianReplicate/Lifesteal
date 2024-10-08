c9b1006b4325077f3c3e3d42561b01d6ac3bd2ba
LoxiGoose <docgoosenn@gmail.com>
2024-10-07 17:48:11 -0700

    cleaning up my ocd further

--- fabric/src/main/java/mc/mian/lifesteal/fabric/event/LSEvents.java
+++ fabric/src/main/java/mc/mian/lifesteal/fabric/event/LSEvents.java
@@ -1,5 +1,6 @@
 package mc.mian.lifesteal.fabric.event;
 
+import mc.mian.lifesteal.util.LSConstants;
 import net.fabricmc.fabric.api.entity.event.v1.ServerPlayerEvents;
 import mc.mian.lifesteal.data.LSData;
 import net.minecraft.resources.ResourceLocation;
@@ -8,7 +9,7 @@ import java.util.Collection;
 
 public class LSEvents {
     public static void register() {
-        LifeSteal.LOGGER.debug("Registering ModEvents for " + LifeSteal.MOD_ID);
+        LSConstants.LOGGER.debug("Registering ModEvents for " + LSConstants.MOD_ID);
         ServerPlayerEvents.COPY_FROM.register(((oldPlayer, newPlayer, alive) -> LSData.get(oldPlayer).ifPresent(oldData -> LSData.get(newPlayer).ifPresent(newData ->
         {
             Collection<ResourceLocation> keys = newData.getKeys();
