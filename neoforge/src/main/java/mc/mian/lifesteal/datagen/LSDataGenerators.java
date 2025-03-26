50d7c8ddb8c98348e93f574a2f5cc9c62f37f20d
Mian <docgoosenn@gmail.com>
2025-03-25 20:20:34 -0700

    Removed redundant code

--- neoforge/src/main/java/mc/mian/lifesteal/datagen/LSDataGenerators.java
+++ neoforge/src/main/java/mc/mian/lifesteal/datagen/LSDataGenerators.java
@@ -1,14 +1,10 @@
 package mc.mian.lifesteal.datagen;
 
-import mc.mian.lifesteal.common.block.LSBlocks;
-import mc.mian.lifesteal.common.item.LSItems;
-import mc.mian.lifesteal.util.LSConstants;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.loot.LootTableProvider;
 import net.minecraft.data.tags.TagsProvider;
-import net.minecraft.server.packs.PackType;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
@@ -18,9 +14,6 @@ import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 public class LSDataGenerators {
-//    private static final String PATH_ITEM_PREFIX = "textures/item";
-//    private static final String PATH_BLOCK_PREFIX = "textures/block";
-//    private static final String PATH_SUFFIX = ".png";
 
     @SubscribeEvent
     public static void generateData(GatherDataEvent.Client ev) {
@@ -28,9 +21,6 @@ public class LSDataGenerators {
         final DataGenerator gen = ev.getGenerator();
         final PackOutput packOutput = gen.getPackOutput();
 
-//        addVirtualPackContents(efh);
-
-//        if (ev.includeServer()) {
         gen.addProvider(true, new LSWorldGenProvider(packOutput, provider)); // ConfiguredFeatures&PlacedFeatures with BiomeModifiers && Structures
         gen.addProvider(true, new LSRecipesProvider.Runner(packOutput, provider)); // Recipes
         gen.addProvider(true, new LootTableProvider(packOutput, Collections.emptySet(), // LootTables
@@ -44,34 +34,6 @@ public class LSDataGenerators {
         gen.addProvider(true, new LSItemTagsProvider(packOutput, provider, tagsProvider.contentsGetter()));
         gen.addProvider(true, new LSLangProvider(packOutput));
         gen.addProvider(true, new LSModelProvider(packOutput));
-//        gen.addProvider(true, new LSItemModelProvider(packOutput));
-//        }
     }
 
-//    private static void addVirtualPackContents(ExistingFileHelper existingFileHelper) {
-//        existingFileHelper.trackGenerated(
-//                LSConstants.modLoc(LSBlocks.CRYSTAL_BLOCK.getId().getPath()), PackType.CLIENT_RESOURCES, PATH_SUFFIX, PATH_BLOCK_PREFIX
-//        );
-//        existingFileHelper.trackGenerated(
-//                LSConstants.modLoc(LSBlocks.DEEPSLATE_CRYSTAL_ORE.getId().getPath()), PackType.CLIENT_RESOURCES, PATH_SUFFIX, PATH_BLOCK_PREFIX
-//        );
-//        existingFileHelper.trackGenerated(
-//                LSConstants.modLoc(LSBlocks.CRYSTAL_ORE.getId().getPath()), PackType.CLIENT_RESOURCES, PATH_SUFFIX, PATH_BLOCK_PREFIX
-//        );
-//        existingFileHelper.trackGenerated(
-//                LSConstants.modLoc(LSBlocks.NETHERRACK_CRYSTAL_ORE.getId().getPath()), PackType.CLIENT_RESOURCES, PATH_SUFFIX, PATH_BLOCK_PREFIX
-//        );
-//        existingFileHelper.trackGenerated(
-//                LSConstants.modLoc(LSItems.CRYSTAL_CORE.getId().getPath()), PackType.CLIENT_RESOURCES, PATH_SUFFIX, PATH_ITEM_PREFIX
-//        );
-//        existingFileHelper.trackGenerated(
-//                LSConstants.modLoc(LSItems.HEART_CRYSTAL.getId().getPath()), PackType.CLIENT_RESOURCES, PATH_SUFFIX, PATH_ITEM_PREFIX
-//        );
-//        existingFileHelper.trackGenerated(
-//                LSConstants.modLoc(LSItems.CRYSTAL_FRAGMENT.getId().getPath()), PackType.CLIENT_RESOURCES, PATH_SUFFIX, PATH_ITEM_PREFIX
-//        );
-//        existingFileHelper.trackGenerated(
-//                LSConstants.modLoc(LSItems.REVIVE_CRYSTAL.getId().getPath()), PackType.CLIENT_RESOURCES, PATH_SUFFIX, PATH_ITEM_PREFIX
-//        );
-//    }
 }
