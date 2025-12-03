63452b0e6b3e0b51e95621b15a9f278935a44df2
Mian <docgoosenn@gmail.com>
2025-12-03 01:26:50 -0800

    1.21.10 port finished :D

--- neoforge/src/main/java/mc/mian/lifesteal/datagen/LSLootProvider.java
+++ neoforge/src/main/java/mc/mian/lifesteal/datagen/LSLootProvider.java
@@ -10,14 +10,17 @@ import net.minecraft.data.loot.LootTableSubProvider;
 import net.minecraft.data.loot.packs.VanillaBlockLoot;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.util.context.ContextKey;
+import net.minecraft.util.context.ContextKeySet;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.storage.loot.LootPool;
 import net.minecraft.world.level.storage.loot.LootTable;
 import net.minecraft.world.level.storage.loot.entries.LootItem;
 import net.minecraft.world.level.storage.loot.functions.CopyComponentsFunction;
 import net.minecraft.world.level.storage.loot.functions.EnchantWithLevelsFunction;
 import net.minecraft.world.level.storage.loot.functions.SetItemCountFunction;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
 import net.minecraft.world.level.storage.loot.predicates.LootItemRandomChanceCondition;
 import net.minecraft.world.level.storage.loot.providers.number.ConstantValue;
 import net.minecraft.world.level.storage.loot.providers.number.UniformGenerator;
@@ -36,8 +39,9 @@ public class LSLootProvider {
             this.add(LSBlocks.CRYSTAL_ORE.get(), (block) -> createOreDrop(block, LSItems.CRYSTAL_FRAGMENT.get()));
             this.add(LSBlocks.DEEPSLATE_CRYSTAL_ORE.get(), (block) -> createOreDrop(block, LSItems.CRYSTAL_FRAGMENT.get()));
             this.add(LSBlocks.NETHERRACK_CRYSTAL_ORE.get(), (block) -> createOreDrop(block, LSItems.CRYSTAL_FRAGMENT.get()));
+
             this.add(LSBlocks.REVIVE_HEAD.get(), (block) -> LootTable.lootTable().withPool((LootPool.lootPool().add(LootItem.lootTableItem(LSItems.REVIVE_HEAD_ITEM.get())
-                    .apply(CopyComponentsFunction.copyComponentsFromBlockEntity(new ContextKey<>(LSBlocks.REVIVE_HEAD.getId()))
+                    .apply(CopyComponentsFunction.copyComponentsFromBlockEntity(LootContextParams.BLOCK_ENTITY)
                             .include(DataComponents.PROFILE).include(DataComponents.NOTE_BLOCK_SOUND))))));
             dropSelf(LSBlocks.CRYSTAL_BLOCK.get());
         }
